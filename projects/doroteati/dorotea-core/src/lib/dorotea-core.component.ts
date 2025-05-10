25c20524658ff5c63732ba74977c3589c8c4a556
HeiderHDev <heiderhrnndz@gmail.com>
2025-05-10 11:39:59 -0500

    Initial commit

---
+++ projects/doroteati/dorotea-core/src/lib/dorotea-core.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-dorotea-core',
+  imports: [],
+  template: `
+    <p>
+      dorotea-core works!
+    </p>
+  `,
+  styles: ``
+})
+export class DoroteaCoreComponent {
+
+}
---
+++ projects/doroteati/dorotea-core/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of dorotea-core
+ */
+
+export * from './lib/dorotea-core.service';
+export * from './lib/dorotea-core.component';
